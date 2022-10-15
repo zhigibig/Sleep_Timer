9f49424c304c94997684fa3b95ebe91b8f50b6df
mr_libertariat <95529471+zhigibig@users.noreply.github.com>
2022-10-15 15:16:29 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stdio.h>
+
+struct Time {
+  int hours;
+  int minutes;
+};
+
+int sleep_time(); 
+
+int main() {
+  using namespace std;
+
+  int arr = sleep_time();
+
+  cout << arr << endl;
+
+  return 0;
+};
+
+int sleep_time() {
+  // this function is getting time from user
+  
+  using namespace std;
+
+  int digits[10];
+
+  for(int i = 0; i < 10; i++) {
+    digits[i] = i;  
+  };
+
+  // how function can return the list?
+  return 0;
+};
